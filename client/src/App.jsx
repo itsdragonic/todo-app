faeff68c75f97d7ba575080c3c5edb8e2b73623c
itsdragonic <fzawislak25@lrchs.org>
2026-05-21 17:07:29 -0500

    added URL to production backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:3000'
+const API = 'https://todo-app-server-1frd.onrender.com/'
 
 function App() {
   const [todos, setTodos] = useState([])
